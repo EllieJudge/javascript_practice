414e8f8b5343c52a7ca3aecc4e6a5c39bda4aa29
Ellie Judge <ellie.judge@peak.ai>
2020-02-18 19:04:36 +0000

    Completed weeks 1 and 2 of the js challenges

--- challenges/week1.js
+++ challenges/week1.js
@@ -1,54 +1,82 @@
 function capitalize(word) {
   if (word === undefined) throw new Error("word is required");
   // Add your code here!
+  return word.charAt(0).toUpperCase() + word.slice(1);
 }
 
 function generateInitials(firstName, lastName) {
   if (firstName === undefined) throw new Error("firstName is required");
   if (lastName === undefined) throw new Error("lastName is required");
   // Add your code here!
+  // return firstName.charAt(0) + '.' + lastName.charAt(0);
+  return `${firstName.charAt(0)}.${lastName.charAt(0)}`;
 }
 
 function addVAT(originalPrice, vatRate) {
   if (originalPrice === undefined) throw new Error("originalPrice is requied");
   if (vatRate === undefined) throw new Error("vatRate is required");
   // Add your code here!
+  const vat = (originalPrice / 100) * vatRate;
+  return parseFloat((originalPrice + vat).toFixed(2));
 }
 
 function getSalePrice(originalPrice, reduction) {
   if (originalPrice === undefined) throw new Error("originalPrice is required");
   if (reduction === undefined) throw new Error("reduction is required");
   // Add your code here!
+  const reducedBy = (originalPrice / 100) * reduction;
+  return parseFloat((originalPrice - reducedBy).toFixed(2));
 }
 
 function getMiddleCharacter(str) {
   if (str === undefined) throw new Error("str is required");
   // Add your code here!
+  //get length
+  const len = str.length;
+
+  if (len % 2 === 0) {
+    const even = len / 2;
+    return str.slice((even - 1), (even + 1));
+  }
+  else {
+    return str.substring((len / 2), (len / 2) + 1);
+  }
 }
 
 function reverseWord(word) {
   if (word === undefined) throw new Error("word is required");
   // Add your code here!
+  return word.split('').reverse().join('');
 }
 
 function reverseAllWords(words) {
   if (words === undefined) throw new Error("words is required");
   // Add your code here!
+  return words.map(el => el.split('').reverse().join(''));
 }
 
 function countLinuxUsers(users) {
   if (users === undefined) throw new Error("users is required");
   // Add your code here!
+  let count = 0;
+  users.forEach(obj => Object.values(obj).forEach(el => el === 'Linux' ? count++ : 0));
+  return count;
 }
 
 function getMeanScore(scores) {
   if (scores === undefined) throw new Error("scores is required");
   // Add your code here!
+  return Number(parseFloat((scores.reduce((a, b) => a + b)) / scores.length).toFixed(2));
 }
 
 function simpleFizzBuzz(n) {
   if (n === undefined) throw new Error("n is required");
   // Add your code here!
+  if (n % 3 === 0 && n % 5 === 0) return 'fizzbuzz';
+  if (n % 3 === 0) return 'fizz';
+  if (n % 5 === 0) return 'buzz';
+  return n;
+
 }
 
 module.exports = {
--- challenges/week2.js
+++ challenges/week2.js
@@ -1,26 +1,34 @@
 function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
   // Your code here!
+  return sandwich.fillings;
 }
 
 function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
   // Your code here!
+  return person.city === 'Manchester';
 }
 
 function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
   // Your code here!
+  return Math.ceil(people / 40);
 }
 
 function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
   // Your code here!
+  let count = 0;
+  arr.forEach(el => el === 'sheep' ? count++ : '');
+  return count;
 }
 
 function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
   // Your code here!
+  const patt = /M[0-9]/ig;
+  return person.address.postCode.match(patt) !== null;
 }
 
 module.exports = {
